3bc130687cd90292bb31ef86d86af1cf67a9d64f
Necmi Hasan <hasan@innoscale.de>
2022-10-03 19:40:52 +0200

    UPDATE connection-provider.ts: refactored code

--- src/websocket/connection-provider.ts
+++ src/websocket/connection-provider.ts
@@ -1,4 +1,3 @@
-import {MissingConnectionInformationException} from '../exception/missing-connection-information.exception';
 import {ClientOptions} from '../interface/client-options.interface';
 import * as WebSocket from 'ws';
 
@@ -19,25 +18,15 @@ export class ConnectionProvider {
    * @constructor
    * @param {ClientOptions} options
    */
-  constructor(private readonly options?: ClientOptions) {}
+  constructor(private readonly options: ClientOptions) {}
 
   /**
    * @returns {Promise<string>}
    */
   public getEndpoint(): string {
-    if (this.options) {
-      return `${this.options.ssl ? 'wss' : 'ws'}://${this.options.host}:${
-        this.options.port
-      }/rpc`;
-    }
-
-    if (!process.env.SURREAL_HOST || !process.env.SURREAL_PORT) {
-      throw new MissingConnectionInformationException();
-    }
-
-    return `${Boolean(JSON.parse(process.env.SURREAL_SSL)) ? 'wss' : 'ws'}://${
-      process.env.SURREAL_HOST
-    }:${process.env.SURREAL_PORT}/rpc`;
+    return `${this.options.ssl ? 'wss' : 'ws'}://${this.options.host}:${
+      this.options.port
+    }/rpc`;
   }
 
   /**
