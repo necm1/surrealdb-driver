6938c2c115dafac2410afb8b17f80074ebb853e2
Necmi Hasan <hasan@innoscale.de>
2022-10-03 17:30:24 +0200

    UPDATE connection-provider.ts: added exception for missing connection information

--- src/websocket/connection-provider.ts
+++ src/websocket/connection-provider.ts
@@ -1,4 +1,5 @@
-import {ClientOptions} from 'src/interface/client-options.interface';
+import {MissingConnectionInformationException} from '../exception/missing-connection-information.exception';
+import {ClientOptions} from '../interface/client-options.interface';
 import * as WebSocket from 'ws';
 
 /**
@@ -21,13 +22,17 @@ export class ConnectionProvider {
   constructor(private readonly options?: ClientOptions) {}
 
   /**
-   * @returns {string}
+   * @returns {Promise<string>}
    */
   public getEndpoint(): string {
     if (this.options) {
       return `${this.options.ssl ? 'wss' : 'ws'}://${this.options.host}:${
         this.options.port
       }/rpc`;
+    }
+
+    if (!process.env.SURREAL_HOST || !process.env.SURREAL_PORT) {
+      throw new MissingConnectionInformationException();
     }
 
     return `${Boolean(JSON.parse(process.env.SURREAL_SSL)) ? 'wss' : 'ws'}://${
