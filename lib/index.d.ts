c8a98253281b911a2718317a0b78e0fc3bd029fa
necm1 <necmi.m.hasan@outlook.de>
2022-10-07 02:44:54 +0200

    CREATE index.ts

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+export * from './surreal';
+export * from './websocket/connection-handler';
+export * from './websocket/connection-provider';
+export * from './interface/client-options.interface';
+export * from './interface/record.interface';
+export * from './interface/request-queue.interface';
+export * from './interface/response.interface';
